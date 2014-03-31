b8e74f1e6ec3d2f97c7b5f1ef5968b5bb3daa839
Jun NOGATA <nogajun@gmail.com>
2014-03-31 13:16:21 +0900

    add package rsync, systemd and fix grub parameter

--- packer/wheezy/scripts/base.sh
+++ packer/wheezy/scripts/base.sh
@@ -9,4 +9,6 @@ chown -R vagrant:vagrant /home/vagrant/.ssh
 
 # set GRUB_TIMEOUT is none
 sed -i '/^GRUB_TIMEOUT/s/[0-9]$/0/g' /etc/default/grub
+sed -i 's/frontend=noninteractive//g' /etc/default/grub
+sed -i 's/quiet/quiet\ init=\/bin\/systemd/g' /etc/default/grub
 /usr/sbin/update-grub
