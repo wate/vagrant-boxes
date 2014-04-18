d2673abc53d872808018baf58f3e83ef506fd79b
Jun NOGATA <nogajun@gmail.com>
2014-04-18 09:47:56 +0900

    stopped to use systemd

--- packer/wheezy/scripts/base.sh
+++ packer/wheezy/scripts/base.sh
@@ -10,5 +10,5 @@ chown -R vagrant:vagrant /home/vagrant/.ssh
 # set GRUB_TIMEOUT is none
 sed -i '/^GRUB_TIMEOUT/s/[0-9]$/0/g' /etc/default/grub
 sed -i 's/frontend=noninteractive//g' /etc/default/grub
-sed -i 's/quiet/quiet\ init=\/bin\/systemd/g' /etc/default/grub
+# If use in systemd # sed -i 's/quiet/quiet\ init=\/bin\/systemd/g' /etc/default/grub
 /usr/sbin/update-grub
