4713624fc037f8cbf5296cc45544bbc4931967dc
wate <pc.poisoning@gmail.com>
2015-06-02 00:39:04 +0900

    sudoersを調整

--- scripts/centos/pre-base.sh
+++ scripts/centos/pre-base.sh
@@ -2,6 +2,3 @@
 
 echo 'RES_OPTIONS="single-request-reopen"' >> /etc/sysconfig/network
 service network restart
-
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
-sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
