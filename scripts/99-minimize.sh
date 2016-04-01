dcc3a99afdd94704b7815c9c7ce0ae46a97ef8e2
wate <pc.poisoning@gmail.com>
2016-04-02 04:42:06 +0900

    共通のプロビジョニング処理を微調整

--- scripts/99-minimize.sh
+++ scripts/99-minimize.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -eux
 
-# Whiteout the swap partition to reduce box size 
-# Swap is disabled till reboot 
+# Whiteout the swap partition to reduce box size
+# Swap is disabled till reboot
 readonly swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
 readonly swappart=$(readlink -f /dev/disk/by-uuid/"$swapuuid")
 /sbin/swapoff "$swappart"
-dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? is suppressed" 
+dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? is suppressed"
 /sbin/mkswap -U "$swapuuid" "$swappart"
 
 dd if=/dev/zero of=/EMPTY bs=1M
