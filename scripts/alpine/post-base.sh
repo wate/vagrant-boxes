d29a14c9ed2539affb8cd8cb0a7e45ba99463601
wate <pc.poisoning@gmail.com>
2016-04-03 02:14:17 +0900

    プロビジョニング処理を調整

--- scripts/alpine/post-base.sh
+++ scripts/alpine/post-base.sh
@@ -4,13 +4,12 @@ apk update && apk upgrade
 
 source /etc/os-release
 
-cat << EOF
-$NAME $VERSION_ID
+cat << EOF >/etc/motd
+Welcome to ${PRETTY_NAME}
 
-See the Alpine Wiki for how-to guides and
-general information about administrating
-Alpine systems and development.
-See <http://wiki.alpinelinux.org>
+The Alpine Wiki contains a large amount of how-to guides and general
+information about administrating Alpine systems.
+See <http://wiki.alpinelinux.org>.
 EOF
 
 # sshd setting
@@ -19,8 +18,19 @@ sed -i '/^PermitRootLogin yes/d' /etc/ssh/sshd_config
 # install packages
 apk add curl bash sudo
 
-adduser -D vagrant
+adduser vagrant -D -s /bin/bash
 echo "vagrant:vagrant" | chpasswd
 
-# sudo
-echo "%vagrant ALL=NOPASSWD:ALL" >> /etc/sudoers
+# sudo setting
+echo "includedir /etc/sudoers.d" >> /etc/sudoers
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/vagrant
+
+# add apk community repo
+VARSION=$(echo ${PRETTY_NAME} | tr -d "${NAME} ")
+echo "http://mirror.leaseweb.com/alpine/${VARSION}/community" >>/etc/apk/repositories
+
+# set keymap
+/sbin/setup-keymap jp jp106
+
+# set timezone
+/sbin/setup-timezone -z Asia/Tokyo
