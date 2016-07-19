0f7af757969a1b8641c8e0a1010b63783683b2ab
wate <pc.poisoning@gmail.com>
2016-07-20 01:57:50 +0900

    update

--- scripts/02-vagrant.sh
+++ scripts/02-vagrant.sh
@@ -2,6 +2,6 @@
 
 # Install vagrant keys
 mkdir -p -m 0700 /home/vagrant/.ssh
-wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+wget -O /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
