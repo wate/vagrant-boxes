f6a512680ea9e27605f940e941e2509beb326d30
wate <pc.poisoning@gmail.com>
2016-09-24 02:08:12 +0900

    centos用の設定を更新

--- scripts/centos/post-cleanup.sh
+++ scripts/centos/post-cleanup.sh
@@ -1,14 +1,42 @@
 #!/bin/bash -eux
 
-yum -y remove gcc kernel-devel kernel-headers perl
+# Remove development and kernel source packages
+yum -y remove gcc cpp kernel-devel kernel-headers perl;
 yum -y clean all
 
-# clean up redhat interface persistence
-rm -f /etc/udev/rules.d/70-persistent-net.rules
+# Clean up network interface persistence
+rm -f /etc/udev/rules.d/70-persistent-net.rules;
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+rm -rf /dev/.udev/;
 
-for ndev in $(ls /etc/sysconfig/network-scripts/ifcfg-*); do
-  if [ "$(basename ${ndev})" != "ifcfg-lo" ]; then
-    sed -i '/^HWADDR/d' ${ndev}
-    sed -i '/^UUID/d' ${ndev}
+# radio off & remove all interface configration
+nmcli radio all off
+/bin/systemctl stop NetworkManager.service
+
+for ifcfg in `ls -1 /etc/sysconfig/network-scripts/ifcfg-* | grep -v ifcfg-lo`; do
+  rm -f $ifcfg
+done
+rm -rf /var/lib/NetworkManager/*
+
+
+echo "==> Setup /etc/rc.d/rc.local for CentOS7"
+cat <<_EOF_ | cat >> /etc/rc.d/rc.local
+#PACKER-BEGIN
+LANG=C
+# delete all connection
+for con in \`nmcli -t -f uuid con\`; do
+  if [ "\$con" != "" ]; then
+    nmcli con del \$con
   fi
 done
+# add gateway interface connection.
+gwdev=\`nmcli dev | grep ethernet | egrep -v 'unmanaged' | head -n 1 | awk '{print \$1}'\`
+if [ "\$gwdev" != "" ]; then
+  nmcli c add type eth ifname \$gwdev con-name \$gwdev
+fi
+sed -i "/^#PACKER-BEGIN/,/^#PACKER-END/d" /etc/rc.d/rc.local
+chmod -x /etc/rc.d/rc.local
+#PACKER-END
+_EOF_
+chmod +x /etc/rc.d/rc.local
