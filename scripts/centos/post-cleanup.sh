d04f909aa9a1638e0c57027601c9b1e55b66cace
wate <pc.poisoning@gmail.com>
2017-07-23 15:43:41 +0900

    70-persistent-net.rulesのクリーンアップ処理を調整

--- scripts/centos/post-cleanup.sh
+++ scripts/centos/post-cleanup.sh
@@ -6,7 +6,7 @@ yum -y clean all
 
 # Clean up network interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
-mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
 rm -rf /dev/.udev/;
 
