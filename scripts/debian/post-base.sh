fea7cb6712fe777b47b81fc1bef2e649f9a666b4
wate <pc.poisoning@gmail.com>
2018-11-11 19:21:48 +0900

    stretchのイメージを更新

--- scripts/debian/post-base.sh
+++ scripts/debian/post-base.sh
@@ -5,9 +5,31 @@
 grep -q 'secure_path' /etc/sudoers \
   || sed -i -e '/Defaults\s\+env_reset/a Defaults\tsecure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' /etc/sudoers;
 
-# Adding a 2 sec delay to the interface up, to make the dhclient happy
-echo "pre-up sleep 2" >> /etc/network/interfaces;
+debian_version="`lsb_release -r | awk '{print $2}'`";
+major_version="`echo $debian_version | awk -F. '{print $1}'`";
+
+# Disable systemd apt timers/services
+if [ "$major_version" -ge "9" ]; then
+  systemctl stop apt-daily.timer;
+  systemctl stop apt-daily-upgrade.timer;
+  systemctl disable apt-daily.timer;
+  systemctl disable apt-daily-upgrade.timer;
+  systemctl mask apt-daily.service;
+  systemctl mask apt-daily-upgrade.service;
+  systemctl daemon-reload;
+fi
+
+# Disable periodic activities of apt
+cat <<EOF >/etc/apt/apt.conf.d/10periodic;
+APT::Periodic::Enable "0";
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
 
 # Set up password-less sudo for the vagrant user
 echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/vagrant;
 chmod 440 /etc/sudoers.d/vagrant;
+
+apt-get install -y sudo;
