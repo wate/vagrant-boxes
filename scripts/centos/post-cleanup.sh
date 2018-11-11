8d4f73c4719d9d6381c3b3f32fddeb2feb45deca
wate <pc.poisoning@gmail.com>
2018-11-11 15:36:26 +0900

    クリーンアップ用スクリプトを調整

--- scripts/centos/post-cleanup.sh
+++ scripts/centos/post-cleanup.sh
@@ -5,10 +5,17 @@ yum -y remove gcc cpp kernel-devel kernel-headers perl
 yum -y clean all
 
 # Clean up network interface persistence
-rm -f /etc/udev/rules.d/70-persistent-net.rules
-ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
-rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
-rm -rf /dev/.udev/
+rm -f /etc/udev/rules.d/70-persistent-net.rules;
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+rm -rf /dev/.udev/;
+
+for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
+    if [ "`basename $ndev`" != "ifcfg-lo" ]; then
+        sed -i '/^HWADDR/d' "$ndev";
+        sed -i '/^UUID/d' "$ndev";
+    fi
+done
 
 # radio off & remove all interface configration
 nmcli radio all off
@@ -18,7 +25,6 @@ for ifcfg in `ls -1 /etc/sysconfig/network-scripts/ifcfg-* | grep -v ifcfg-lo`; 
   rm -f $ifcfg
 done
 rm -rf /var/lib/NetworkManager/*
-
 
 echo "==> Setup /etc/rc.d/rc.local for CentOS7"
 cat <<_EOF_ | cat >> /etc/rc.d/rc.local
